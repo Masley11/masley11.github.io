d024a4565b77cad1ce58e53657b8f558359fe139
Masley11 <fatawlamoukiri11@gmail.com>
2025-06-09 15:08:59 +0000

    corection de sw.js

--- sw.js
+++ sw.js
@@ -3,19 +3,40 @@ const filesToCache = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/style.css',   // si tu mets ton CSS séparé, sinon ignore
-  '/icon-192.png',
-  '/icon-512.png'
+  '/icon',
+  '/maskable.png'
+  'monochrome.png'
 ];
 
+// Installation : on met en cache les fichiers listés, en ignorant les erreurs individuelles
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      return cache.addAll(filesToCache);
+    caches.open(cacheName).then(async (cache) => {
+      for (const file of filesToCache) {
+        try {
+          await cache.add(file);
+        } catch (err) {
+          console.warn('Échec du caching du fichier:', file, err);
+        }
+      }
     })
   );
 });
 
+// Activation : nettoyage des anciennes caches
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter(key => key !== cacheName)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+// Interception des requêtes : réponse par le cache si possible, sinon fetch réseau
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
